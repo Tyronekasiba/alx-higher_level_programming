299bdae7e18aa62de7306c1ee25703739be93198
Tyronekasiba <tyronekasiba@gmail.com>
2023-03-08 07:33:01 -0800

    range loop of 2 different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for j in range(0,10):
-    for x in range(1,10):
+for j in range(0, 10):
+    for x in range(1, 10):
         if j >= x:
             continue
         elif j == 8 and x == 9:
