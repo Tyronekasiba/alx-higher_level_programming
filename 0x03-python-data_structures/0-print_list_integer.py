8f745232a72834a94904568d84e6b17a77d600d3
Tyronekasiba <tyronekasiba@gmail.com>
2023-03-13 07:54:50 -0700

    loop through a list in py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for y in my_list:
-        print("{}".format(y))
+    for y in range(len(my_list)):
+        print("{:d}".format(my_list[y]))
