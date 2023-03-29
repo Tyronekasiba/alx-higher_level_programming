c8f1b2fb52c0eb9594d5f8819e348037c06c73f1
Tyronekasiba <tyronekasiba@gmail.com>
2023-03-28 18:48:26 -0700

    a class square that defines a square base on a private arttribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+
+    """define a class square """
+    def __init__(self,size):
+        """define a constructor method that takes a size parameter | the doble underscore makes the class arttribute private"""
+        self.__size = size
