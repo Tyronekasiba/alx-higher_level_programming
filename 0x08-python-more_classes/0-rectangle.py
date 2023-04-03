f48b5bd5e500b6d4c962505a282e997e49de9876
Tyronekasiba <tyronekasiba@gmail.com>
2023-04-03 13:49:01 -0700

    classes in py | define a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,9 +2,10 @@
 """ A class tha defines a retagle """
 
 class Rectangle:
-    def __init__(self):
-        pass
-    def get_area(self):
-        return 0
-R = Rectangle()
-print(R.get_area())
+    """ represent a rectangle """
+    #def __init__(self):
+    pass
+    #def get_area(self):
+ #       return 0
+#R = Rectangle()
+#print(R.get_area())
