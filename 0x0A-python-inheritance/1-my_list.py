8d844e9eb19b1508c4e8233d49f5e78aca0e151f
Tyronekasiba <tyronekasiba@gmail.com>
2023-04-13 12:16:30 -0700

    a class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class my_list that inherits from the list
+"""
+
+class MyList(list):
+    """ 
+    a class that represent MyList
+    """
+
+
+
+    def print_sorted(self):
+        """
+        prints the list , sorted
+        """
+        print(sorted(self))
+    
