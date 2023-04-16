71d88b2ebd111e87ad2d8fd9ed5aaffaebe3cebc
Tyronekasiba <tyronekasiba@gmail.com>
2023-04-15 19:17:51 -0700

    a function that reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,6 @@ function that writes a string to a text file in utf-8 and returns the number of 
 
 
 def write_file(filename="", text=""):
-    """
-    writes a text file in utf-8 and returns number of charactors in stdout
-    """
+    """writes a text file in utf-8 and returns number of charactors in stdout"""
     with open(filename, 'w', encoding='utf-8') as f :
         return f.write(text)
