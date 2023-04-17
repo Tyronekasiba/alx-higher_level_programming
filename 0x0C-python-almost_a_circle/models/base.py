6efab54509e80992377a8d5d02c689eef6037c5d
Tyronekasiba <tyronekasiba@gmail.com>
2023-04-17 13:49:59 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-Base class for all models
+define a Base class
 """
 
+
+
 class Base:
-    __nb_objects = 0  # a private class attribute
+    """ intialize class Base """
+
+
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Construct a Base class.
-
-        Args:
-        id (int, optional): unique identifier for the instance, default to None
-        """
-
         if id is not None:
-            # if id is provided, assign to public instance attribute
             self.id = id
         else:
-            # if id is not provided, increment nb_objects and assign to public
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
