2ed9d63c57a479c2e94a80c91365aefb1789bc19
Tyronekasiba <tyronekasiba@gmail.com>
2023-04-17 13:53:48 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,17 +3,21 @@
 define a Base class
 """
 
-
-
 class Base:
-    """ intialize class Base """
-
+    """ initialize class Base """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Construct a Base instance.
+
+        Args:
+            id (int, optional): unique identifier for the instance, defaults to None
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
