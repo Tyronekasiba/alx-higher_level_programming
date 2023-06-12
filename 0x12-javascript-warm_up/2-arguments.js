244f83637fe6767bb4b98e0d74525fcf9ad3abfd
Tyronekasiba <tyronekasiba@gmail.com>
2023-06-12 14:21:21 -0700

    else and  else if in js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// number of arguments
+
+const NumArgv = process.argv.length - 2;
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
