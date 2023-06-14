e60a8bc8213dc25816c554758e52ce37f4660d01
Tyronekasiba <tyronekasiba@gmail.com>
2023-06-14 03:09:01 -0700

    loops in javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// prints 3 lines using loops in JavaScript
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myVar.length; i++) {
+  console.log(myVar[i]);
+}
